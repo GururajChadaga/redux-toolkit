a70a65cac32f7c36c1b893de184e2d59f0536c1d
Gururaj Chadaga <gururajchadaga@Gururajs-MacBook-Air.local>
2023-02-13 23:03:33 +0530

    React-RTK setup

---
+++ react-rtk-demo/src/features/cake/CakeView.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const CakeView = () => {
+  return (
+    <div>
+      <h2>Number of cakes -</h2>
+      <button>Order a Cake</button>
+      <button>Restock Cakes</button>
+    </div>
+  );
+};
---
+++ react-rtk-demo/src/features/icecream/iceCreamSlice.js
@@ -0,0 +1,33 @@
+import { ordered as cakeOrdered } from '../cake/cakeSlice';
+
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  numOfIceCreams: 10,
+};
+
+const iceCreamSlice = createSlice({
+  name: 'iceCream',
+  initialState,
+  reducers: {
+    ordered: (state) => {
+      state.numOfIceCreams--;
+    },
+    restocked: (state, action) => {
+      state.numOfIceCreams += action.payload;
+    },
+  },
+  //   extraReducers: {
+  //     ['cake/ordered']: (state, action) => {
+  //       state.numOfIceCreams--;
+  //     },
+  //   },
+  extraReducers: (builder) => {
+    builder.addCase(cakeOrdered, (state, actions) => {
+      state.numOfIceCreams--;
+    });
+  },
+});
+
+export default iceCreamSlice.reducer;
+export const { ordered, restocked } = iceCreamSlice.actions;
